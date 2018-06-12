a30d50aa8e43014a72edd06c2c4d4d92b5b947b3
tadahaya <cnb.jpn@gmail.com>
2018-06-12 18:53:03 +0900

    migration file

--- db/migrate/20180612094418_create_groups.rb
+++ db/migrate/20180612094418_create_groups.rb
@@ -1,7 +1,7 @@
 class CreateGroups < ActiveRecord::Migration[5.0]
   def change
     create_table :groups do |t|
-
+      t.string :name, null: false, index: ture, unique: true
       t.timestamps
     end
   end
