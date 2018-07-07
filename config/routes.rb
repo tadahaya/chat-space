b1ba6d6713f23185faa30d39071f6440981ebb67
tadahaya <cnb.jpn@gmail.com>
2018-07-07 19:50:32 +0900

    編集機能での対応

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'
   resources :users, only: [:edit, :update, :index]
-  resources :groups, only: [:index, :new, :create, :edit, :update] do
+  resources :groups, only: [:index, :new, :create, :edit, :update, :patch] do
     resources :messages, only: [:index, :create]
     resources :users, only: [:index]
     end
